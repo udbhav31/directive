24d93a04eb58f09881bc9c67eb94f0f3b8a35eb8
Udbhav Srivastava <US00433490@techmahindra.com>
2018-10-01 20:38:39 +0530

    notes in js

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 angular.module("formWarmup",[]);
+/*
+the name directive is important in input element
+the binding wich happens is 
+<name attribute of form><name attribute of input><$ which we want to check ie $error,$touched><directive>
 
+the ng-show works when we return false
+
+need to check further about the $ propertites for name attribute
+
+like $valid , $pristin, $dirty, $error, $touched, $untouched
+$pending
+
+
+ ng-model-options="{ updateOn: 'blur' }"
+ ng-model-options="{ updateOn: 'mousedown blur' }
+ ng-model-options="{ debounce: 500 }"
+ ng-model-options="{ updateOn: 'default blur', debounce: { default: 500, blur: 0 } 
+---use it along with ng-model-----
+ <input type="text" ng-model="user.name" ng-model-options="{ debounce: 250 }" /></label><br />
+*/
 
 
 document.addEventListener("DOMContentLoaded", function(){
